8d8228c6bf762cf361e366e7f441d3d705e17da1
Lijie Zhao <zlj501307603@gmail.com>
2017-09-28 12:06:03 -0500

    Add coordinates and regions 06:29

--- myPizzaHistoryMap/ViewController.swift
+++ myPizzaHistoryMap/ViewController.swift
@@ -7,19 +7,78 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    
+    /* --- Coordinate information -----
+     Lat,long
+     Naples: 40.8367321,14.2468856
+     New York: 40.7216294 , -73.995453
+     Chicago: 41.892479 , -87.6267592
+     Chatham: 42.4056555,-82.1860369
+     Beverly Hills: 34.0674607,-118.3977309
+     
+     -->Challenges!!<----
+     208 S. Beverly Drive Beverly Hills CA:34.0647691,-118.3991328
+     2121 N. Clark St Chicago IL: 41.9206921,-87.6375361
+     
+     For region monitoring:
+     latitude: 37.33454235, longitude: -122.03666775000001
+     --- */
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateMapRegion(rangeSpan: 500)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //Propertieis and outlets
+    var coordinate2D = CLLocationCoordinate2DMake(40.8367321, 14.2468856)
+    
+    //Outlets
+    @IBOutlet weak var changePitch: UIButton!
+    @IBOutlet weak var changeMapType: UIButton!
+    @IBOutlet weak var MapView: MKMapView!
+    
+    //Actions
+    @IBAction func changeMapType(_ sender: UIButton) {
     }
-
-
+    
+    @IBAction func changePitch(_ sender: UIButton) {
+    }
+    
+    @IBAction func toggleMapFeatures(_ sender: UIButton) {
+    }
+    
+    @IBAction func findHere(_ sender: UIButton) {
+    }
+    
+    @IBAction func findPizza(_ sender: UIButton) {
+    }
+    
+    @IBAction func locationPicker(_ sender: UISegmentedControl) {
+        let index = sender.selectedSegmentIndex
+        switch index {
+        case 0: //Naples
+            coordinate2D = CLLocationCoordinate2DMake(40.8367321, 14.2468856)
+        case 1: //New York
+            coordinate2D = CLLocationCoordinate2DMake(40.7216294, -73.995453)
+        case 2: //Chicago
+            coordinate2D = CLLocationCoordinate2DMake(41.892479, -87.6267592)
+        case 3: //Chatham
+            coordinate2D = CLLocationCoordinate2DMake(42.4056555, -82.1860369)
+        case 4: //Beverly Hills
+            coordinate2D = CLLocationCoordinate2DMake(34.0674607, -118.3977309)
+        default:
+            coordinate2D = CLLocationCoordinate2DMake(40.8367321, 14.2468856)
+        }
+    }
+    
+    
+    //Instance Methods
+    func updateMapRegion(rangeSpan:CLLocationDistance) {
+        let region = MKCoordinateRegionMakeWithDistance(coordinate2D, rangeSpan, rangeSpan)
+        MapView.region = region
+    }
 }
 
